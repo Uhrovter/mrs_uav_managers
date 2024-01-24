3f8380a063327b476bd8d2fcd0ec466496586518
Azureta <azeret152@gmail.com>
2024-01-24 14:01:33 +0100

    goto relative finished

--- test/control_manager/goto_fcu_service/test.cpp
+++ test/control_manager/goto_fcu_service/test.cpp
@@ -1,14 +1,19 @@
 #include <gtest/gtest.h>
-
+#include <tuple>
+#include <string>
 #include <mrs_uav_testing/test_generic.h>
 
 class Tester : public mrs_uav_testing::TestGeneric {
 
 public:
   bool test();
+  mrs_lib::ServiceClientHandler<mrs_msgs::Vec4> sch_goto_fcu_;
+  bool gotoFcu(const double &x, const double &y, const double &z, const double &hdg);
+
 };
 
 bool Tester::test() {
+  sch_goto_fcu_ = mrs_lib::ServiceClientHandler<mrs_msgs::Vec4>(nh_, "/" + _uav_name_ + "/control_manager/goto_fcu");
 
   {
     auto [success, message] = activateMidAir();
@@ -20,12 +25,13 @@ bool Tester::test() {
   }
 
   {
-    auto [success, message] = this->gotoAbs(0, 0, 2.0, 0);
+    bool success = this->gotoFcu(0.5, 0, 2.0, 0);
 
     if (!success) {
-      ROS_ERROR("[%s]: goto failed with message: '%s'", ros::this_node::getName().c_str(), message.c_str());
+      ROS_ERROR("[%s]: goto failed", ros::this_node::getName().c_str());
       return false;
     }
+
   }
 
   this->sleep(5.0);
@@ -37,6 +43,45 @@ bool Tester::test() {
     return false;
   }
 }
+
+  bool Tester::gotoFcu(const double &x, const double &y, const double &z, const double &hdg) {
+    mrs_msgs::Vec4 srv;
+    auto start_pose = sh_estim_manager_diag_.getMsg()->pose.position;
+    auto start_hdg  = mrs_lib::AttitudeConverter(sh_estim_manager_diag_.getMsg()->pose.orientation).getHeading();
+
+      srv.request.goal[0] = x;
+      srv.request.goal[1] = y;
+      srv.request.goal[2] = z;
+      srv.request.goal[3] = hdg;
+
+      {
+        bool service_call = sch_goto_fcu_.call(srv);
+
+        if (!service_call || !srv.response.success) {
+          return false;
+        }
+      }
+      // | -------------------- check for result -------------------- |
+
+    while (true) {
+
+      if (!ros::ok()) {
+        return false;
+      }
+
+      if (!isFlyingNormally()) {
+        return false;
+      }
+
+      if (isAtPosition(start_pose.x + x, start_pose.y + y, start_pose.z + z, start_hdg + hdg, 0.1)) {
+        return true;
+      }
+
+      sleep(0.01);
+    }
+
+    return false;
+  }
 
 
 TEST(TESTSuite, test) {
